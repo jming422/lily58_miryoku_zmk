0cbf1785202f2dea28b366a7aff7cf0a35c334b1
Jonathan Ming <jming422@gmail.com>
2023-08-22 12:58:03 -0700

    minor right side customizations

--- miryoku/mapping/58/lily58.h
+++ miryoku/mapping/58/lily58.h
@@ -12,8 +12,8 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-&kp ESC   &kp N1  &kp N2  &kp N3       &kp N4  &kp N5                                   &kp N6  &kp N7  &kp N8       &kp N9  &kp N0  &kp GRAVE \
-&kp TAB       K00     K01     K02          K03     K04                                      K05     K06     K07          K08     K09 &kp MINUS \
+&kp ESC   &kp N1  &kp N2  &kp N3       &kp N4  &kp N5                                   &kp N6  &kp N7  &kp N8       &kp N9  &kp N0  &kp EQUAL \
+&kp TAB       K00     K01     K02          K03     K04                                      K05     K06     K07          K08     K09 &kp BSLH  \
 &kp LCTRL     K10     K11     K12          K13     K14                                      K15     K16     K17          K18     K19 &kp SEMI  \
 &kp LSHFT     K20     K21     K22          K23     K24 &kp C_PLAY_PAUSE &kp PAUSE_BREAK     K25     K26     K27          K28     K29 &kp RSHFT \
                           &kp C_BRI_DN     K32     K33     K34              K35             K36     K37 &kp C_BRI_UP
